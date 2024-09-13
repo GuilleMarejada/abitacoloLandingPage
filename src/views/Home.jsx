f51e824080832e44152ec4a3b882b67006e1878c
Longodios <tusaromix@gmail.com>
2024-09-13 13:31:14 +0200

    Creación Footer y assets

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Contacto from "../components/Contacto";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <p className="text-2xl">Home</p>
-      <Contacto />
+      <Contacto/>
+      <Footer/>
+     
     </div>
   );
 };
