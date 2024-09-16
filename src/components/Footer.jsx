577f9bf7832f18acfbb93052da1200cce6448156
Longodios <tusaromix@gmail.com>
2024-09-16 10:42:55 +0200

    Footer finalizado y añado la fuente Railway

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,54 @@
 import React from "react";
 
-const Footer = () =>  {
-    return(
-        <div className="h-60 relative bg-[#919e32] flex">
-            <img className="h-[68px] left-[115px] top-[157.40px] absolute flex-col justify-start items-start gap-4 inline-flex" src="/src/assets/LogoAbitacoloFooter.png"></img>
-            
+const Footer = () => {
+  return (
+    <div className="w-[1640px] h-60  bg-[#919e32] p-3 ">
+      <div className="flex items-center justify-between gap-[45px]">
+        <img
+          className="w-[328.99px] h-[93px]"
+          src="/src/assets/LogoAbitacoloFooter.png"
+          alt="Logo Abitacolo"
+        ></img>
 
-            <div className="flex justify-start">
-                <span className="text-white">POLITICA DE PRIVACIDAD</span>
-                <span className="text-white">POLITICA DE COOKIES</span>
-                <div className="">
-                <img src="/src/assets/IconX.svg"></img>
-                <img src="/src/assets/IconIG.svg"></img>
-                <img src="/src/assets/IconLinkdn.svg"></img>
-                </div>
-            </div>
-            
-
+        <div className="w-[690.77px] h-[26px] justify-start items-center gap-[45px]  inline-flex">
+          <p className="text-white cursor-pointer text-sm ">
+            POLITICA DE PRIVACIDAD
+          </p>
+          <p className="text-white cursor-pointer text-sm ">
+            POLITICA DE COOKIES
+          </p>
+          <div className="flex gap-5">
+            <img
+              className="w-6 h-6"
+              src="/src/assets/IconIG.svg"
+              alt="Instagram"
+            ></img>
+            <img
+              className="w-6 h-6"
+              src="/src/assets/IconLinkdn.svg"
+              alt="LinkedIn"
+            ></img>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className=" border w-[1335px] mt-2  border-solid border-white ml-[75px]"></div>
+      <div className="w-[875px] h-[68px] p-5 ml-[50px] mt-[25px] flex-col justify-start items-start gap-4 inline-flex">
 
+      <div className="self-stretch justify-start items-center gap-7 inline-flex">
+        <p className="text-white text-sm">¿Que es Abitacolo?</p>
+        <p className="text-white text-sm">Nuestro equipo</p>
+        <p className="text-white text-sm">Hitos Abitacolo</p>
+        <p className="text-white text-sm">Nuestros Productos</p>
+        <p className="text-white text-sm">Contacts</p>
+      </div>
+      
+        <p className="text-white text-sm text-center mt-[5px]">© 2024 Abitacolo. All rights reserved.</p>
+      
 
-export default Footer
+      </div>
+      
+    </div>
+  );
+};
+
+export default Footer;
