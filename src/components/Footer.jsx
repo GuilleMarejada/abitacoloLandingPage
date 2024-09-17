a819989eec2f57a26370a532151fcf1c767ffb46
Longodios <tusaromix@gmail.com>
2024-09-17 13:04:51 +0200

    Merge del Footer con Develop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,26 +6,6 @@ import LogoAbitacolo from "../assets/LogoAbitacoloFooter.png";
 const Footer = () => {
   return (
     <div className="w-full  h-[240px] grid justify-center  bg-[#919e32] p-3 ">
-<<<<<<< HEAD
-        <div className="max-w-[1440px]">
-          <div className="flex items-center justify-between gap-[45px]">
-        <img
-          className="w-[328.99px] h-[93px]"
-          src={LogoAbitacolo}
-          alt="Logo Abitacolo"
-        ></img>
-
-        <div className="max-w-[690.77px] h-[26px] justify-start items-center gap-[45px]  inline-flex">
-          <p className="text-white cursor-pointer text-sm ">
-            POLITICA DE PRIVACIDAD
-          </p>
-          <p className="text-white cursor-pointer text-sm ">
-            POLITICA DE COOKIES
-          </p>
-          <div className="flex gap-5">
-            <img className="w-6 h-6" src={IconIG} alt="Instagram"></img>
-            <img className="w-6 h-6" src={IconLinkdn} alt="LinkedIn"></img>
-=======
       <div className="max-w-[1440px]">
         <div className="flex items-center justify-between gap-[45px]">
           <img
@@ -45,7 +25,6 @@ const Footer = () => {
               <img className="w-6 h-6" src={IconIG} alt="Instagram"></img>
               <img className="w-6 h-6" src={IconLinkdn} alt="LinkedIn"></img>
             </div>
->>>>>>> ded1ee603bcce612c37aeebcfe7c6b6b26919c6e
           </div>
         </div>
         <div className=" border mt-2  border-solid border-white mx-[75px] mr-[150px]"></div>
@@ -63,10 +42,6 @@ const Footer = () => {
           </p>
         </div>
       </div>
-<<<<<<< HEAD
-      </div>
-=======
->>>>>>> ded1ee603bcce612c37aeebcfe7c6b6b26919c6e
     </div>
   );
 };
