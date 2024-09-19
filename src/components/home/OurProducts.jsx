48475a583ece653d7d062b4fa692d526b6c1f90b
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-09-19 13:40:33 +0100

    Trabajamos en componentes Hitos, OurProducts y Footer mejorando responsive y estructura

--- src/components/home/OurProducts.jsx
+++ src/components/home/OurProducts.jsx
@@ -29,21 +29,24 @@ const OurProducts = () => {
   ];
 
   return (
-    <div id="products" className="w-full bg-[#1c1c1e] grid justify-center">
-      <div className="h-[700px] w-[1440px] px-8 pt-12 pb-32 bg-[#1c1c1e] flex flex-col gap-8">
+    <div
+      id="products"
+      className="w-full bg-[#1c1c1e] grid justify-center min-h-[700px] pb-6"
+    >
+      <div className="max-w-[1440px] w-full px-8 pt-12 bg-[#1c1c1e] flex flex-col gap-8">
         <div className="py-4 border-b border-white/10 grid justify-center">
           <h2 className="text-white text-2xl font-semibold font-['Raleway'] uppercase">
             Nuestros productos
           </h2>
         </div>
-        <div className="grid grid-cols-2 gap-8">
+        <div className="grid xl:grid-cols-2 gap-8">
           {productInfo.map((product, index) => (
             <div
               key={index}
               className="h-52 bg-white/5 flex items-center gap-8"
             >
               <img
-                className="w-80 h-52"
+                className="max-w-80 h-52"
                 src={product.image}
                 alt={product.title}
               />
@@ -51,7 +54,7 @@ const OurProducts = () => {
                 <h3 className="text-white text-xl font-semibold font-['Raleway'] uppercase">
                   {product.title}
                 </h3>
-                <p className="w-[296px] text-white/40 text-base font-normal font-['Raleway'] uppercase">
+                <p className="max-w-[296px] text-white/40 text-base font-normal font-['Raleway'] uppercase">
                   {product.description}
                 </p>
               </div>
